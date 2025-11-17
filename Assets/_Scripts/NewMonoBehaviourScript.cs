a4967a88aeab5937bef4038e58a0532a68cafee3
Micahiscoo <88602571+Micahiscoo@users.noreply.github.com>
2025-11-16 21:48:06 -0600

    hello world

--- Assets/_Scripts/NewMonoBehaviourScript.cs
+++ Assets/_Scripts/NewMonoBehaviourScript.cs
@@ -3,8 +3,26 @@ using UnityEngine.SceneManagement;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
-    public void clickedButton()
+    TutorialCheck TC;
+
+    private void Start()
+    {
+        TC = GetComponent<TutorialCheck>();
+    }
+    public void clickedButton() //load main menu
     {
         SceneManager.LoadScene(0);
+    }
+
+    public void clickedButton2() //load office
+    {
+        if (TC.canSkip)
+        {
+            SceneManager.LoadScene(1);
+        }
+       else
+        {
+            SceneManager.LoadScene(2);
+        }
     }
 }
